9c85b39c28e15f40d6fe751ec76dd400d590fa1b
miyanokomiya <tomoya.hstexze24632@gmail.com>
2020-08-25 22:41:07 +0900

    lap list

--- lib/edit_workout.dart
+++ lib/edit_workout.dart
@@ -17,17 +17,36 @@ class _EditWorkoutState extends State<EditWorkout> {
     });
   }
 
+  void _startEditLap(int lapIndex) {}
+  void _save(BuildContext context) {
+    final snackBar =
+        SnackBar(content: Text('Saved !!', style: TextStyle(fontSize: 24)));
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.workout.name),
-      ),
+      appBar: AppBar(title: Text(widget.workout.name), actions: [
+        Builder(
+          builder: (BuildContext context) {
+            return RaisedButton.icon(
+              icon: Icon(Icons.save),
+              label: Text('SAVE'),
+              color: Colors.transparent,
+              textColor: Colors.white,
+              onPressed: () => _save(context),
+            );
+          },
+        ),
+      ]),
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
             children: widget.workout.lapItemList
-                .map((lapItem) => getLapItemWidget(lapItem))
+                .asMap()
+                .entries
+                .map((e) => getLapItemWidget(
+                    e.key, e.value, () => {_startEditLap(e.key)}))
                 .toList()),
       ),
       floatingActionButton: FloatingActionButton(
@@ -39,10 +58,44 @@ class _EditWorkoutState extends State<EditWorkout> {
   }
 }
 
-Widget getLapItemWidget(LapItem lapItem) {
-  return Column(children: [
-    Text(lapItem.name),
-    Text(lapItem.time.toString()),
-    Text(lapItem.rest.toString()),
-  ]);
+Widget getLapItemWidget(int index, LapItem lapItem, Function onEdit) {
+  return Container(
+      decoration: const BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.black))),
+      child: Row(children: [
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(12),
+            child: Column(children: [
+              Row(children: [
+                Expanded(
+                  child: Text((index + 1).toString() + '. ' + lapItem.name,
+                      style: TextStyle(fontSize: 24)),
+                ),
+                Text(lapItem.time.toString() + 's',
+                    style: TextStyle(fontSize: 18)),
+              ]),
+              Divider(
+                color: Colors.black,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(right: 12),
+                    child: Text('Rest', style: TextStyle(fontSize: 18)),
+                  ),
+                  Text(lapItem.rest.toString() + 's',
+                      style: TextStyle(fontSize: 18)),
+                ],
+              )
+            ]),
+          ),
+        ),
+        IconButton(
+          color: Colors.lightBlue,
+          onPressed: onEdit,
+          icon: Icon(Icons.edit),
+        )
+      ]));
 }
