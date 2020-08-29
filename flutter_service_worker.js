b0022af1ce24a8cc9abeb40e7e3a1f9a9b748a01
miyanokomiya <miyanokomiya@users.noreply.github.com>
2020-08-29 07:49:43 +0000

    deploy: 8cb434531f26ff8db99b785264f716975f91f90d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "8d510b1a6866b58e160b365ca3efbda5",
+"main.dart.js": "52e3e952660f03f5530275775d5431fe",
 "index.html": "26e96cbc41d4fd91e486b757222e5109",
 "/": "26e96cbc41d4fd91e486b757222e5109",
 "manifest.json": "9a587529e581567bfbf529157fa3366b",
