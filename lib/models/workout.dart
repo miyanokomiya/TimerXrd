ad2d44bd08bba3aaed5d88ad9e2d81eeb60ef32d
miyanokomiya <tomoya.hstexze24632@gmail.com>
2020-08-30 10:47:51 +0900

    connect sqlite

--- lib/models/workout.dart
+++ lib/models/workout.dart
@@ -1,14 +1,30 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 class Workout {
+  int id;
   String name;
   List<LapItem> lapItemList;
 
-  Workout({this.name = 'Workout', this.lapItemList = const []});
+  Workout({this.name = 'Workout', this.lapItemList = const [], this.id});
+
+  Workout.fromMap(Map map) {
+    id = map['id'] as int;
+    name = map['name'] as String;
+  }
 
   Workout clone() {
-    final ret = Workout();
-    ret.name = name;
-    ret.lapItemList = lapItemList.map((l) => l.clone()).toList();
-    return ret;
+    return Workout(
+        id: id,
+        name: name,
+        lapItemList: lapItemList.map((l) => l.clone()).toList());
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+    };
   }
 }
 
@@ -26,11 +42,54 @@ class LapItem {
 
   LapItem({this.name = 'Lap', this.time = 45, this.rest = 15});
 
-  LapItem clone() {
-    final ret = LapItem();
-    ret.name = name;
-    ret.time = time;
-    ret.rest = rest;
-    return ret;
+  LapItem.fromMap(Map map) {
+    name = map['name'] as String;
+    time = map['time'] as int;
+    rest = map['rest'] as int;
   }
+
+  LapItem clone() {
+    return LapItem(name: name, time: time, rest: rest);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'time': time,
+      'rest': rest,
+    };
+  }
+}
+
+Database _database;
+
+Future<Database> getDataBase() async {
+  final databasesPath = await getDatabasesPath();
+  final path = join(databasesPath, 'app.db');
+  // await deleteDatabase(path);
+
+  final database = _database ??
+      await openDatabase(
+        join(path),
+        version: 1,
+        onCreate: (db, version) async {
+          await db.execute("""
+        CREATE TABLE workout(
+          id INTEGER PRIMARY KEY,
+          name TEXT
+        );
+        """);
+          await db.execute("""
+        CREATE TABLE lap_item(
+          workout_id INTEGER,
+          item_index INTEGER,
+          name TEXT,
+          time INTEGER,
+          rest INTEGER,
+          PRIMARY KEY(workout_id, item_index)
+        );
+        """);
+        },
+      );
+  return database;
 }
--- lib/store/workout_store.dart
+++ lib/store/workout_store.dart
@@ -2,29 +2,75 @@ import 'package:flutter/material.dart';
 import '../models/workout.dart';
 
 class WorkoutStore with ChangeNotifier {
-  List<Workout> workoutList = [
-    Workout(
-      name: 'New Workout',
-      lapItemList: [
-        LapItem(name: 'New Lap'),
-        LapItem(name: 'New Lap'),
-      ],
-    )
-  ];
+  List<Workout> workoutList = [];
 
-  void addWorkspace() {
-    workoutList.add(Workout(
-      name: 'New Workout',
-      lapItemList: [
-        LapItem(name: 'New Lap'),
-        LapItem(name: 'New Lap'),
-      ],
-    ));
+  WorkoutStore() : super() {
+    loadValue();
+  }
+
+  Future<void> loadValue() async {
+    final db = await getDataBase();
+    final List<Map> maps = await db.rawQuery('SELECT * FROM workout');
+    final List<Map> lapItemMaps =
+        await db.rawQuery('SELECT * FROM lap_item ORDER BY item_index ASC');
+
+    workoutList = List.generate(maps.length, (i) {
+      final id = maps[i]['id'] as int;
+      return Workout(
+          id: id,
+          name: maps[i]['name'] as String,
+          lapItemList: lapItemMaps
+              .where((element) => element['workout_id'] == id)
+              .map((e) => LapItem.fromMap(e))
+              .toList());
+    });
+
     notifyListeners();
   }
 
-  void updateWorkspace(int index, Workout next) {
-    workoutList[index] = next;
+  Future<void> addWorkspace() async {
+    final workout = Workout(
+      name: 'New Workout',
+      lapItemList: [
+        LapItem(name: 'New Lap'),
+        LapItem(name: 'New Lap'),
+      ],
+    );
+
+    final db = await getDataBase();
+    await db.transaction((txn) async {
+      final workoutId = await txn.insert('Workout', workout.toMap());
+      workout.lapItemList.asMap().entries.forEach((e) async {
+        final map = e.value.toMap();
+        map['workout_id'] = workoutId;
+        map['item_index'] = e.key;
+        await txn.insert('lap_item', map);
+      });
+      workout.id = workoutId;
+    });
+
+    workoutList.add(workout);
+    notifyListeners();
+  }
+
+  Future<void> updateWorkspace(int itemIndex, Workout workout) async {
+    print(workout.id);
+    final db = await getDataBase();
+    await db.transaction((txn) async {
+      await txn.update('Workout', workout.toMap(),
+          where: 'id = ?', whereArgs: [workout.id]);
+      await txn
+          .delete('lap_item', where: 'workout_id = ?', whereArgs: [workout.id]);
+      final batch = txn.batch();
+      workout.lapItemList.asMap().entries.forEach((e) async {
+        final map = e.value.toMap();
+        map['workout_id'] = workout.id;
+        map['item_index'] = e.key;
+        batch.insert('lap_item', map);
+      });
+      await batch.commit();
+    });
+    workoutList[itemIndex] = workout;
     notifyListeners();
   }
 }
