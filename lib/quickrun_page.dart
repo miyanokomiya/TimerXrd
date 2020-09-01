3abc5c709abf485300a7205c9c5739a9593f5ac4
miyanokomiya <tomoya.hstexze24632@gmail.com>
2020-09-01 22:01:00 +0900

    save quick run settings

--- lib/quickrun_page.dart
+++ lib/quickrun_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import './edit_lap_dialog.dart';
 import './models/workout.dart';
 import './run_workout.dart';
@@ -12,9 +13,43 @@ class _QuickrunPageState extends State<QuickrunPage> {
   int draftTime = 45;
   int draftRest = 15;
   int draftRepeat = 10;
+  SharedPreferences prefs;
+  bool ready = false;
+  final String prefix = 'quick_run';
+
+  @override
+  void initState() {
+    super.initState();
+    _initPrefs();
+  }
+
+  Future<void> _initPrefs() async {
+    try {
+      final _prefs = await SharedPreferences.getInstance();
+      setState(() {
+        prefs = _prefs;
+        draftTime = prefs.getInt('$prefix:time') ?? draftTime;
+        draftRest = prefs.getInt('$prefix:rest') ?? draftRest;
+        draftRepeat = prefs.getInt('$prefix:repeat') ?? draftRepeat;
+      });
+    } catch (_) {
+      setState(() {
+        ready = true;
+      });
+      rethrow;
+    }
+  }
+
+  void _save(String key, int value) {
+    if (prefs != null) {
+      prefs.setInt('$prefix:$key', value);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (!ready && prefs == null) return Scaffold(body: Container());
+
     return Scaffold(
         body: Column(
           children: [
@@ -22,24 +57,27 @@ class _QuickrunPageState extends State<QuickrunPage> {
                 padding: const EdgeInsets.all(10),
                 child: const Text('Quick Run', style: TextStyle(fontSize: 36))),
             Padding(
-              padding: const EdgeInsets.only(left: 10),
+              padding: const EdgeInsets.only(left: 20, right: 20),
               child: Column(
                 children: [
                   getTimeSelectField('Time', draftTime, (int next) {
                     setState(() {
                       draftTime = next;
+                      _save('time', next);
                     });
                   }),
                   const Divider(),
                   getTimeSelectField('Rest', draftRest, (int next) {
                     setState(() {
                       draftRest = next;
+                      _save('rest', next);
                     });
                   }),
                   const Divider(),
                   getTimeSelectField('Repeat', draftRepeat, (int next) {
                     setState(() {
                       draftRepeat = next;
+                      _save('repeat', next);
                     });
                   }, count: 30, step: 1, unit: ''),
                   const Divider(),
