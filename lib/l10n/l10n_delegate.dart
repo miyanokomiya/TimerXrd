b31567ff65e1bdb9f2798bd76a96ab993fcabab9
miyanokomiya <tomoya.hstexze24632@gmail.com>
2020-09-08 01:37:45 +0900

    fix l10n

--- lib/l10n/l10n_delegate.dart
+++ lib/l10n/l10n_delegate.dart
@@ -6,7 +6,7 @@ class L10nDelegate extends LocalizationsDelegate<L10n> {
   const L10nDelegate();
 
   @override
-  bool isSupported(Locale locale) => ['ja'].contains(locale.languageCode);
+  bool isSupported(Locale locale) => ['en', 'ja'].contains(locale.languageCode);
 
   @override
   Future<L10n> load(Locale locale) => L10n.load(locale);
